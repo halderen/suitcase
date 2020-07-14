34ec8242c164a9820322c9191215179c2d57582d
halderen <berry@nlnetlabs.nl>
2020-07-14 15:18:39 +0200

    proper statup loop

--- main.c
+++ main.c
@@ -20,6 +20,9 @@ int
 main(int argc, char* argv[])
 {
     pid_t pid;
+    int optdaemon = 0;
+    int pipefd[2];
+    int ch;
     (void)argc;
     (void)argv;
     settings_handle cfghandle = NULL;
@@ -31,16 +34,42 @@ main(int argc, char* argv[])
         ++argv0;
 
     settings_configure(&cfghandle, QUOTE(SYSCONFDIR), PACKAGE_NAME ".conf", -1);
-    pid = checkpidfile(PACKAGE_NAME ".pid");
     settings_access(NULL, 0, NULL);
-    pid = daemonize(NULL);
-    if (pid<0) {
-        exit(1);
-    } else if(pid>0) {
-        writepidfile(PACKAGE_NAME ".pid", pid);
-        exit(0);
+
+    /* Steps to act as a daemon process*/
+    if (optdaemon) {
+        pid = checkpidfile(PACKAGE_NAME ".pid");
+        if (pid) {
+            fprintf(stderr,"%s already running as process %d\n",argv0,pid);
+            exit(1);
+        }
+        pipe(pipefd);
+        pid = daemonize(NULL);
+        if (pid<0) {
+            exit(1);
+        } else if(pid>0) {
+            close(pipefd[1]);
+            writepidfile(PACKAGE_NAME ".pid", pid);
+            read(pipefd[0], &ch, 1);
+            if (ch != 0) {
+                while(ch>1 && ch<255) {
+                    write(2, &ch, 1);
+                    read(pipefd[0], &ch, 1);
+                }
+                close(pipefd[0]);
+                exit(1);
+            }
+            close(pipefd[0]);
+            exit(0);
+        }
+        close(pipefd[0]);
+        if (droppriviledges(NULL, NULL)) {
+            write(pipefd[1], "\001", 1);
+        }
+        write(pipefd[1], "", 1);
+        close(pipefd[1]);
     }
-    droppriviledges(NULL, NULL);
+
     /* do stuff */
     exit(0);
 }
